e51de1668caa16b476d02680dfb331c4458fe10b
PaulXu <xuyihan5150@gmail.com>
2020-07-10 10:53:10 +0800

    Hello Algorithm

---
+++ src/xu/paul/Main.java
@@ -0,0 +1,13 @@
+package xu.paul;
+
+/**
+ * @author: Paul Xu
+ * @createdAt: 2020/7/10 10:32
+ * @description: main
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello Algorithm");
+  }
+}
